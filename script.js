8748fdb481679506326d7cf8424a73242d7c6663
Abosh Upadhyaya <abosh@Aboshs-MacBook-Air.local>
2022-01-13 14:13:06 -0800

    initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+document.getElementById('convert').addEventListener('click', () => {
+  const input = document.getElementById('content').value.split(' ')
+  let output = ''
+  const vowels = new Set(['A', 'a', 'E', 'e', 'I', 'i', 'O', 'o', 'U', 'u'])
+  const consonants = new Set(['P', 'p'])
+  for (const token of input) {
+    if (token.length > 3) {
+      let consonant = false
+      for (let i = 0; i < token.length - 1; i++) {
+        if (!vowels.has(token[i])) {
+          output += ' ' + token.substring(0, i) + '🅿️' + token.substring(i + 1)
+          consonant = true
+          break;
+        }
+      }
+      if (!consonant) {
+        output += ' ' + token
+      }
+    } else {
+      output += ' ' + token
+    }
+  }
+  console.log(output.substring(1))
+})
