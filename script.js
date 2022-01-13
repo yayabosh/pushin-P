a8dc79ffb28c35bf6bee01f28c4fd170f29623fb
Abosh Upadhyaya <aboshu@gmail.com>
2022-01-13 15:03:49 -0800

    fixed #1

--- script.js
+++ script.js
@@ -1,16 +1,25 @@
 document.getElementById('convert').addEventListener('click', () => {
   const input = document.getElementById('content').value.split(' ')
   let output = ''
-  const vowels = new Set(['A', 'a', 'E', 'e', 'I', 'i', 'O', 'o', 'U', 'u'])
-  const consonants = new Set(['P', 'p'])
+  const consonants = new Set([
+    'B', 'b', 'C', 'c', 'D', 'd', 'G', 'g', 'K', 'k', 'P', 'p', 'Q', 'q', 'T', 't', 'X', 'x'
+  ])
   for (const token of input) {
-    if (token.length > 3) {
+    if (token === 'P' || token === 'p') {
+      output += ' 🅿️'
+    } else if (token.length > 3) {
       let consonant = false
-      for (let i = 0; i < token.length - 1; i++) {
-        if (!vowels.has(token[i])) {
-          output += ' ' + token.substring(0, i) + '🅿️' + token.substring(i + 1)
+      for (let i = 0; i < token.length - 1 && !consonant; i++) {
+        // Hard vs. soft C issue
+        if (token[i] === 'C' || token[i] === 'c') {
+          const next = token[i + 1].toUpperCase()
+          if (next !== 'I' && next !== 'E' && next !== 'Y') {
+            consonant = true
+            output += ' ' + token.substring(0, i) + '🅿️' + token.substring(i + 1)
+          }
+        } else if (consonants.has(token[i])) {
           consonant = true
-          break;
+          output += ' ' + token.substring(0, i) + '🅿️' + token.substring(i + 1)
         }
       }
       if (!consonant) {
